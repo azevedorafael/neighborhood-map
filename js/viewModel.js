3f3b5c570a6bce7e1d5aa9b8ac8aaca2b429a263
Rafael da S.P. de Azevedo <rafaelsilva.fatec@gmail.com>
2017-10-29 04:35:40 -0200

    Adds style and animation to the markers when clicked

--- js/viewModel.js
+++ js/viewModel.js
@@ -5,6 +5,17 @@ var markers = [];
 
 var i = 0;
 
+// These are the real estate listings that will be shown to the user.
+// Normally we'd have these in a database instead.
+var locations = [
+    { title: 'Park Ave Penthouse', location: { lat: 40.7713024, lng: -73.9632393 } },
+    { title: 'Union Square Open Floor Plan', location: { lat: 40.7347062, lng: -73.9895759 } },
+    { title: 'East Village Hip Studio', location: { lat: 40.7281777, lng: -73.984377 } },
+    { title: 'TriBeCa Artsy Bachelor Pad', location: { lat: 40.7195264, lng: -74.0089934 } },
+    { title: 'Chinatown Homey Space', location: { lat: 40.7180628, lng: -73.9961237 } }
+];
+
+
 function initMap() {
     // Constructor creates a new map - only center and zoom are required.
     map = new google.maps.Map(document.getElementById('map'), {
@@ -12,16 +23,7 @@ function initMap() {
         zoom: 15
     });
 
-    // These are the real estate listings that will be shown to the user.
-    // Normally we'd have these in a database instead.
-    var locations = [
-        { title: 'Park Ave Penthouse', location: { lat: 40.7713024, lng: -73.9632393 } },
-        { title: 'Chelsea Loft', location: { lat: 40.7444883, lng: -73.9949465 } },
-        { title: 'Union Square Open Floor Plan', location: { lat: 40.7347062, lng: -73.9895759 } },
-        { title: 'East Village Hip Studio', location: { lat: 40.7281777, lng: -73.984377 } },
-        { title: 'TriBeCa Artsy Bachelor Pad', location: { lat: 40.7195264, lng: -74.0089934 } },
-        { title: 'Chinatown Homey Space', location: { lat: 40.7180628, lng: -73.9961237 } }
-    ];
+
 
     var largeInfowindow = new google.maps.InfoWindow();
     var bounds = new google.maps.LatLngBounds();
@@ -49,8 +51,8 @@ function initMap() {
     }
     // Extend the boundaries of the map for each marker
     map.fitBounds(bounds);
-    
-ko.applyBindings(viewModel);
+
+    ko.applyBindings(viewModel);
 }
 
 
@@ -70,6 +72,11 @@ function populateInfoWindow(marker, infowindow) {
     }
 }
 
+function openInfoWindow(marker) {
+    // Check to make sure the infowindow is not already opened on this marker.
+    infowindow.open(map, marker);
+}
+
 // This is a simple *viewmodel* - JavaScript that defines the data and behavior of your UI
 
 
@@ -85,16 +92,41 @@ var viewModel = {
     itemsFiltered: ko.observableArray([]),
     filter: ko.observable(""),
     search: ko.observable(""),
+    numberOfClicks: ko.observable(0),
+
     addItem: function () {
-        this.items.push(new Item(title, lat ,lng));
+        this.items.push(new Item(title, lat, lng));
     },
     removeItem: function (item) {
         this.items.remove(item);
+    },
+
+    showInfoWindow: function () {
+        // marker.addListener('click', function () {
+        //     populateInfoWindow(this, largeInfowindow);
+        // });
+        console.log(this.title());
+        for (var i = 0; i < markers.length; i++) {
+            if (map.getBounds().contains(markers[i].getPosition())) {
+                if (markers[i].title == this.title()) {
+                    var markerImage = new google.maps.MarkerImage(
+                        'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ 'FFFF24' +
+                        '|40|_|%E2%80%A2',
+                        new google.maps.Size(21, 34),
+                        new google.maps.Point(0, 0),
+                        new google.maps.Point(10, 34),
+                        new google.maps.Size(21,34));
+                    markers[i].setIcon(markerImage);
+                    markers[i].setAnimation(google.maps.Animation.BOUNCE)
+                    // openInfoWindow(markers[i]);
+                }
+            }
+        }
     }
 };
 
 //startsWith method
-var stringStartsWith = function (string, startsWith) {          
+var stringStartsWith = function (string, startsWith) {
     string = string || "";
     if (startsWith.length > string.length)
         return false;
@@ -106,58 +138,38 @@ var itensFiltered = ko.observableArray([]);
 //ko.utils.arrayFilter - filter the items using the filter text
 this.filteredItems = ko.computed(function () {
     var filter = this.filter().toLowerCase();
+
     if (!filter) {
         ko.utils.arrayFilter(this.items(), function (item) {
 
-            for (var i=0; i<markers.length; i++){
-                if( map.getBounds().contains(markers[i].getPosition()) ){
-                         markers[i].setMap(map);
+            for (var i = 0; i < markers.length; i++) {
+                if (map.getBounds().contains(markers[i].getPosition())) {
+                    markers[i].setMap(map);
                 }
             }
         });
         return this.items();
     } else {
         return ko.utils.arrayFilter(this.items(), function (item) {
-            if (stringStartsWith(item.title().toLowerCase(), filter)){
+            if (stringStartsWith(item.title().toLowerCase(), filter)) {
                 console.log(item.title(), item.lat(), item.lng());
 
-                for (var i=0; i<markers.length; i++){
-                    if( map.getBounds().contains(markers[i].getPosition()) ){
-                        if (markers[i].title != item.title()){
-                            console.log(markers[i].title);
+                for (var i = 0; i < markers.length; i++) {
+                    if (map.getBounds().contains(markers[i].getPosition())) {
+                        if (markers[i].title != item.title()) {
                             markers[i].setMap(null);
                         }
 
                     }
-                };
+                }
                 return stringStartsWith(item.title().toLowerCase(), filter);
             }
         });
     }
 }, viewModel);
 
-
-
-
-
-var JSONdataFromServer = [
-    { title: 'Park Ave Penthouse', location: { lat: 40.7713024, lng: -73.9632393 } },
-    { title: 'Chelsea Loft', location: { lat: 40.7444883, lng: -73.9949465 } },
-    { title: 'Union Square Open Floor Plan', location: { lat: 40.7347062, lng: -73.9895759 } },
-    { title: 'East Village Hip Studio', location: { lat: 40.7281777, lng: -73.984377 } },
-    { title: 'TriBeCa Artsy Bachelor Pad', location: { lat: 40.7195264, lng: -74.0089934 } },
-    { title: 'Chinatown Homey Space', location: { lat: 40.7180628, lng: -73.9961237 } },
-];
-
-// console.log(JSONdataFromServer);
-
-//parse into an object
-// var dataFromServer = ko.utils.parseJson(JSONdataFromServer);
-
-// console.log(dataFromServer);
-
-//do some basic mapping (without mapping plugin)
-var mappedData = ko.utils.arrayMap(JSONdataFromServer, function (item) {
+//do some basic mapping 
+var mappedData = ko.utils.arrayMap(locations, function (item) {
     return new Item(item.title, item.location.lat, item.location.lng);
 });
 
